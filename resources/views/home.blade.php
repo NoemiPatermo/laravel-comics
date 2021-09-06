e20649745ed9fbfcef27ee00a7c74e7eb533a27d
noemi86-cpu <noemipatermo@gmail.com>
2021-09-06 14:35:16 +0200

    in fieri

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -26,6 +26,7 @@
                     <div class="row">
                         @foreach($comics as $comic)
                             <div class="comics-item col-2">
+                                
                              <a href="/card/{{ $loop->index}}">      <!--dopo card arriva una variabile, tramite il loop che puoi usare solo nel foreach, che invii al tuo singolo componente-->
                                 <div class="comics-item-content">
                                     <img src="{{$comic['thumb']}}" alt="single-comic"/>
